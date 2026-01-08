e94c273e83174ba150d956123eeb91962360dc30
raminka13 <raminka13@hotmail.com>
2026-01-08 16:27:13 -0500

    feat: Add clientLogos data for dynamic display of aviation logos

---
+++ src/data/clientLogos.js
@@ -0,0 +1,124 @@
+const clientLogos = [
+  {
+    url: "/logos/Alto-Flight-Academy-logo-2025-2-1.png",
+    name: "Alto Flight Academy",
+    link: "https://altoflight.com/",
+    invert: false,
+  },
+  {
+    url: "/logos/blitz-aviation-logo.webp",
+    name: "Blitz Aviation",
+    link: "https://blitzaviation.com/",
+    invert: false,
+  },
+  {
+    url: "/logos/flytech-logo_1000px.png",
+    name: "FlyTech Pilot Academy",
+    link: "https://flystj.com/",
+    invert: false,
+  },
+  {
+    url: "/logos/Hawkins-Flight-Academy-logo-2.png",
+    name: "Hawkins Flight Academy",
+    link: "https://hawkinsflight.com/",
+    invert: true,
+  },
+  {
+    url: "/logos/nvflight-nobg.webp",
+    name: "NV Flight",
+    link: "https://www.nvflight.com/",
+    invert: false,
+  },
+  {
+    url: "/logos/LAFA-white.webp",
+    name: "LA Flight Academy",
+    link: "https://laflightacademy.com/",
+    invert: false,
+  },
+  {
+    url: "/logos/NG Logo White Background 2.webp",
+    name: "NextGen Flight Academy",
+    link: "https://flyhere.aero/",
+    invert: true,
+  },
+  {
+    url: "/logos/SFA-logo.webp",
+    name: "Summit Flight Academy",
+    link: "https://www.summitflightacademy.com/",
+    invert: false,
+  },
+  {
+    url: "/logos/ideal-logo.webp",
+    name: "Ideal Aviation",
+    link: "https://idealaviationstl.com/",
+    invert: false,
+  },
+  {
+    url: "/logos/high-tide-aviation-logo-gradation.CFCXi2nO.webp",
+    name: "High Tide Aviation",
+    link: "https://hightideaviation.com/",
+    invert: false,
+  },
+  {
+    url: "/logos/sun-city-white-text-logo.webp",
+    name: "Sun City Aviation",
+    link: "https://suncityaviation.com/",
+    invert: false,
+  },
+  {
+    url: "/logos/speedway-flight-training-written-logo.png",
+    name: "Speedway Flight Training",
+    link: "https://speedwayft.com/",
+    invert: false,
+  },
+  {
+    url: "/logos/pitcairn-logo-400-white.webp",
+    name: "Pitcairn Flight Academy",
+    link: "https://pitcairnflightacademy.com/",
+    invert: false,
+  },
+  {
+    url: "/logos/blue-skies-reverse-logo.webp",
+    name: "Blue Skies Above Aviation",
+    link: "https://flytheblueskies.com/",
+    invert: false,
+  },
+  /* {
+    url: "/logos/red-arrow-logo.webp",
+    name: "Red Arrow Flight Academy",
+    link: "https://www.flyredarrow.com",
+    invert: false,
+  }, */
+  /* {
+    url: "/logos/cirrus-logo.webp",
+    name: "Cirrus Aviation",
+    link: "https://www.cirrusaviation.com/",
+    invert: false,
+  }, */
+  {
+    url: "/logos/simplifly_2.webp",
+    name: "Simplifly Flight School",
+    link: "https://simpliflyco.com/",
+    invert: false,
+  },
+  {
+    url: "/logos/Tampa-Bay-Aviation-Logo.webp",
+    name: "Tampa Bay Aviation",
+    link: "https://www.tampabayaviation.com/",
+    invert: false,
+  },
+  {
+    url: "/logos/uft-logo-new.webp",
+    name: "Universal Flight Training",
+    link: "https://www.flyuft.com/",
+    invert: false,
+  },
+  /* {
+    url: "/logos/ignite-flight-academy-logo.webp",
+    name: "Ignite Flight Academy",
+    link: "https://igniteflight.com/",
+    invert: false,
+  }, */
+];
+
+export default clientLogos;
