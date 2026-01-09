c6265f927194d6ba662dc9bd571b0bca5b7dc4af
raminka13 <raminka13@hotmail.com>
2026-01-09 16:03:36 -0500

    feat: Refactor NavbarFlyout component to enhance About Us section and update resource links

--- src/components/NavBarFlyout.jsx
+++ src/components/NavBarFlyout.jsx
@@ -30,6 +30,9 @@ import {
   AcademicCapIcon,
   MicrophoneIcon,
   CubeTransparentIcon,
+  UserPlusIcon,
+  BriefcaseIcon,
+  PresentationChartBarIcon,
 } from "@heroicons/react/24/outline";
 import {
   ChevronDownIcon,
@@ -39,42 +42,27 @@ import {
   RectangleGroupIcon,
 } from "@heroicons/react/20/solid";
 
-const services = [
+const aboutUs = [
   {
-    name: "Web Development",
-    description: "Build responsive and engaging websites",
-    href: "/services/web-development",
-    icon: CodeBracketSquareIcon,
+    name: "Partner with Us",
+    description:
+      "Explore partnership opportunities with Right Rudder Marketing",
+    href: "/about/partnerships",
+    icon: UserPlusIcon,
   },
   {
-    name: "SEO Optimization",
-    description: "Improve your website's visibility on search engines",
-    href: "/services/seo-optimization",
-    icon: MagnifyingGlassCircleIcon,
+    name: "Career Opportunities",
+    description:
+      "Join the Right Rudder Marketing team and help shape the future of aviation marketing",
+    href: "/about/careers",
+    icon: BriefcaseIcon,
   },
   {
-    name: "Video Production",
-    description: "Create engaging video content to captivate your audience",
-    href: "/services/video-production",
-    icon: PlayCircleIconOutline,
-  },
-  {
-    name: "Pay-Per-Click Advertising",
-    description: "Maximize your ROI with targeted ad campaigns",
-    href: "/services/pay-per-click-advertising",
-    icon: CurrencyDollarIcon,
-  },
-  {
-    name: "Flight School CRM",
-    description: "Manage your flight school's operations efficiently",
-    href: "https://flightschoolcrm.com/",
-    icon: UserCircleIcon,
-  },
-  {
-    name: "GMB Management",
-    description: "Optimize and manage your Google My Business profile",
-    href: "/services/gmb-management",
-    icon: BuildingStorefrontIcon,
+    name: "Investor Relations",
+    description:
+      "Learn more about investment opportunities with Right Rudder Marketing",
+    href: "/about/investors",
+    icon: PresentationChartBarIcon,
   },
 ];
 const resources = [
@@ -103,7 +91,7 @@ const resources = [
     icon: MicrophoneIcon,
   },
 ];
-const callsToActionServices = [
+const callsToActionAboutUs = [
   { name: "Watch demo", href: "#", icon: PlayCircleIcon },
   { name: "Contact sales", href: "#", icon: PhoneIcon },
   { name: "View all services", href: "#", icon: RectangleGroupIcon },
@@ -117,7 +105,7 @@ export default function NavbarFlyout() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className="relative top-0 left-0 right-0 isolate z-50 bg-gray-900">
+    <header className="absolute top-0 left-0 right-0 isolate z-50">
       <nav
         aria-label="Global"
         className="mx-auto font-inter flex max-w-7xl items-center justify-between p-6 lg:px-8"
@@ -143,9 +131,25 @@ export default function NavbarFlyout() {
           </button>
         </div>
         <PopoverGroup className="hidden lg:flex lg:gap-x-12">
+          <a
+            href="/marketing-system"
+            className="flex flex-col gap-0 text-center group"
+          >
+            <span className="text-xs/6 p-0 m-0 font-light text-gray-300 group-hover:text-amber-300">
+              Our Marketing
+            </span>
+            <span className="font-extrabold -mt-2 p-0 text-2xl uppercase text-gray-200">
+              System
+            </span>
+          </a>
           <Popover>
-            <PopoverButton className="flex items-center gap-x-1 text-sm/6 font-semibold text-white">
-              Services
+            <PopoverButton className="flex flex-col text-center gap-0 items-center group hover:cursor-pointer">
+              <span className="text-xs/6 p-0 m-0 font-light text-gray-300 group-hover:text-amber-300">
+                Webinars, Magazine, Podcast
+              </span>
+              <span className="font-extrabold -mt-2 p-0 text-2xl uppercase text-gray-200">
+                Resources
+              </span>
               <ChevronDownIcon
                 aria-hidden="true"
                 className="size-5 flex-none text-gray-500"
@@ -154,75 +158,7 @@ export default function NavbarFlyout() {
 
             <PopoverPanel
               transition
-              className="absolute inset-x-0 top-24 bg-gray-900 transition data-closed:-translate-y-1 data-closed:opacity-0 data-enter:duration-200 data-enter:ease-out data-leave:duration-150 data-leave:ease-in"
-            >
-              {/* Presentational element used to render the bottom shadow, if we put the shadow on the actual panel it pokes out the top, so we use this shorter element to hide the top of the shadow */}
-              <div
-                aria-hidden="true"
-                className="absolute inset-0 top-1/2 bg-gray-900 ring-1 ring-white/15"
-              />
-              <div className="relative bg-gray-700">
-                <div className="mx-auto flex flex-wrap justify-center max-w-7xl gap-x-4 px-6 py-10 lg:px-8 xl:gap-x-8">
-                  {services.map((item) => (
-                    <div
-                      key={item.name}
-                      className="group w-1/5 relative rounded-lg p-6 text-sm/6 hover:bg-white/5"
-                    >
-                      <div className="flex size-11 items-center justify-center rounded-lg bg-gray-700/50 group-hover:bg-gray-700">
-                        <item.icon
-                          aria-hidden="true"
-                          className="size-6 text-gray-400 group-hover:text-white"
-                        />
-                      </div>
-                      <a
-                        href={item.href}
-                        target={
-                          item.href.startsWith("http") ? "_blank" : undefined
-                        }
-                        className="mt-6 block font-semibold text-white"
-                      >
-                        {item.name}
-                        <span className="absolute inset-0" />
-                      </a>
-                      <p className="mt-1 text-gray-400">{item.description}</p>
-                    </div>
-                  ))}
-                </div>
-                <div className="bg-gray-800/50">
-                  <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                    <div className="flex justify-center divide-x divide-white/5 border-x border-white/10">
-                      {callsToActionServices.map((item) => (
-                        <a
-                          key={item.name}
-                          href={item.href}
-                          className="flex items-center w-1/3 justify-center gap-x-2.5 p-3 text-sm/6 font-semibold text-white hover:bg-gray-800"
-                        >
-                          <item.icon
-                            aria-hidden="true"
-                            className="size-5 flex-none text-gray-500"
-                          />
-                          {item.name}
-                        </a>
-                      ))}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </PopoverPanel>
-          </Popover>
-
-          <Popover>
-            <PopoverButton className="flex items-center gap-x-1 text-sm/6 font-semibold text-white">
-              Resources
-              <ChevronDownIcon
-                aria-hidden="true"
-                className="size-5 flex-none text-gray-500"
-              />
-            </PopoverButton>
-
-            <PopoverPanel
-              transition
-              className="absolute inset-x-0 top-24 bg-slate-800 transition data-closed:-translate-y-1 data-closed:opacity-0 data-enter:duration-200 data-enter:ease-out data-leave:duration-150 data-leave:ease-in"
+              className="absolute inset-x-0 top-24 bg-slate-800 transition data-closed:-translate-y-2 data-closed:opacity-0 data-enter:duration-200 data-enter:ease-out data-leave:duration-150 data-leave:ease-in"
             >
               {/* Presentational element used to render the bottom shadow, if we put the shadow on the actual panel it pokes out the top, so we use this shorter element to hide the top of the shadow */}
               <div
@@ -234,7 +170,7 @@ export default function NavbarFlyout() {
                   {resources.map((item) => (
                     <div
                       key={item.name}
-                      className="group w-1/5 relative rounded-lg p-6 text-sm/6 hover:bg-slate-900/5"
+                      className="group w-1/5 relative rounded-lg p-6 text-sm/5 hover:bg-slate-900/5"
                     >
                       <div className="flex size-11 items-center justify-center rounded-lg bg-slate-400/50 group-hover:bg-slate-700">
                         <item.icon
@@ -252,7 +188,9 @@ export default function NavbarFlyout() {
                         {item.name}
                         <span className="absolute inset-0" />
                       </a>
-                      <p className="mt-1 text-slate-600">{item.description}</p>
+                      <p className="mt-1 text-slate-500 group-hover:text-slate-700">
+                        {item.description}
+                      </p>
                     </div>
                   ))}
                 </div>
@@ -279,15 +217,94 @@ export default function NavbarFlyout() {
             </PopoverPanel>
           </Popover>
 
-          <a href="#" className="text-sm/6 font-semibold text-white">
-            Blog
+          <a
+            href="/marketing-system"
+            className="flex flex-col gap-0 text-center group"
+          >
+            <span className="text-xs/6 p-0 m-0 font-light text-gray-300 group-hover:text-amber-300">
+              News & Tips
+            </span>
+            <span className="font-extrabold -mt-2 p-0 text-2xl uppercase text-gray-200">
+              Blog
+            </span>
           </a>
-          <a href="#" className="text-sm/6 font-semibold text-white">
-            About Us
-          </a>
+          <Popover>
+            <PopoverButton className="flex flex-col text-center gap-0 items-center hover:cursor-pointer group">
+              <span className="text-xs/6 p-0 m-0 font-light text-gray-300 group-hover:text-amber-300">
+                Proven Results
+              </span>
+              <span className="font-extrabold -mt-2 p-0 text-2xl uppercase text-gray-200">
+                About
+              </span>
+              <ChevronDownIcon
+                aria-hidden="true"
+                className="size-5 flex-none text-gray-500"
+              />
+            </PopoverButton>
+
+            <PopoverPanel
+              transition
+              className="absolute inset-x-0 top-24 bg-slate-800 transition data-closed:-translate-y-2 data-closed:opacity-0 data-enter:duration-200 data-enter:ease-out data-leave:duration-150 data-leave:ease-in"
+            >
+              {/* Presentational element used to render the bottom shadow, if we put the shadow on the actual panel it pokes out the top, so we use this shorter element to hide the top of the shadow */}
+              <div
+                aria-hidden="true"
+                className="absolute inset-0 top-1/2 bg-slate-100 ring-1 ring-white/15"
+              />
+              <div className="relative bg-mariner-200">
+                <div className="mx-auto flex flex-wrap justify-center max-w-7xl gap-x-4 px-6 py-10 lg:px-8 xl:gap-x-8">
+                  {aboutUs.map((item) => (
+                    <div
+                      key={item.name}
+                      className="group w-1/5 relative rounded-lg p-6 text-sm/5 hover:bg-mariner-900/5"
+                    >
+                      <div className="flex size-11 items-center justify-center rounded-lg bg-mariner-200/50 group-hover:bg-mariner-700">
+                        <item.icon
+                          aria-hidden="true"
+                          className="size-6 text-mariner-600 group-hover:text-white"
+                        />
+                      </div>
+                      <a
+                        href={item.href}
+                        target={
+                          item.href.startsWith("http") ? "_blank" : undefined
+                        }
+                        className="mt-6 block font-semibold text-gray-700"
+                      >
+                        {item.name}
+                        <span className="absolute inset-0" />
+                      </a>
+                      <p className="mt-1 text-gray-400 group-hover:text-gray-700">
+                        {item.description}
+                      </p>
+                    </div>
+                  ))}
+                </div>
+                <div className="bg-mariner-800/50">
+                  <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                    <div className="flex justify-center divide-x divide-white/5 border-x border-white/10">
+                      {callsToActionAboutUs.map((item) => (
+                        <a
+                          key={item.name}
+                          href={item.href}
+                          className="flex items-center w-1/3 justify-center gap-x-2.5 p-3 text-sm/6 font-semibold text-mariner-100 hover:bg-mariner-800"
+                        >
+                          <item.icon
+                            aria-hidden="true"
+                            className="size-5 flex-none text-mariner-700"
+                          />
+                          {item.name}
+                        </a>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </PopoverPanel>
+          </Popover>
         </PopoverGroup>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="#" className="btn-primary">
+          <a href="/contact" className="btn-primary">
             Contact Us
           </a>
         </div>
@@ -320,27 +337,12 @@ export default function NavbarFlyout() {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-white/10">
               <div className="space-y-2 py-6">
-                <Disclosure as="div" className="-mx-3">
-                  <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pr-3.5 pl-3 text-base/7 font-semibold text-white hover:bg-white/5">
-                    Services
-                    <ChevronDownIcon
-                      aria-hidden="true"
-                      className="size-5 flex-none group-data-open:rotate-180"
-                    />
-                  </DisclosureButton>
-                  <DisclosurePanel className="mt-2 space-y-2">
-                    {[...services, ...callsToActionServices].map((item) => (
-                      <DisclosureButton
-                        key={item.name}
-                        as="a"
-                        href={item.href}
-                        className="block rounded-lg py-2 pr-3 pl-6 text-sm/7 font-semibold text-white hover:bg-white/5"
-                      >
-                        {item.name}
-                      </DisclosureButton>
-                    ))}
-                  </DisclosurePanel>
-                </Disclosure>
+                <a
+                  href="/marketing-system"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-light text-white hover:bg-white/5"
+                >
+                  Our System
+                </a>
                 <Disclosure as="div" className="-mx-3">
                   <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pr-3.5 pl-3 text-base/7 font-semibold text-white hover:bg-white/5">
                     Resources
@@ -363,22 +365,22 @@ export default function NavbarFlyout() {
                   </DisclosurePanel>
                 </Disclosure>
                 <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-white/5"
+                  href="/blog"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-light text-white hover:bg-white/5"
                 >
                   Blog
                 </a>
                 <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-white/5"
+                  href="/about"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-light text-white hover:bg-white/5"
                 >
                   About Us
                 </a>
               </div>
               <div className="py-6">
                 <a
-                  href="#"
-                  className="btn-primary -mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-white hover:bg-white/5"
+                  href="/contact"
+                  className="btn-primary -mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-light text-white hover:bg-white/5"
                 >
                   Contact Us
                 </a>
