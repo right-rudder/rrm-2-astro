b2b214a754a19dcf137fb289e6ad69d600ac47a5
raminka13 <raminka13@hotmail.com>
2026-01-09 16:03:06 -0500

    feat: Enhance LogosCloud and OurClientsLogos components with improved styling and marquee functionality

--- src/components/OurClientsLogos.jsx
+++ src/components/OurClientsLogos.jsx
@@ -3,36 +3,38 @@ import clientLogos from "../data/clientLogos";
 
 const MarqueeComponent = () => {
   return (
-    <div className="">
-      <Marquee
-        pauseOnHover={true}
-        gradient={true}
-        gradientColor="#1e2939"
-        style={{ zIndex: -20 }}
-      >
-        {clientLogos.map((logo, index) => (
-          <a
-            href={logo.link}
-            key={index}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="group flex justify-center align-middle py-2"
-          >
-            <img
-              src={logo.url}
-              alt={`${logo.name} logo`}
-              className={`min-w-12 md:max-h-16 max-w-24 md:max-w-52 w-full object-contain duration-300 ease-in-out group-hover:scale-110 group-hover:brightness-110 ${
-                logo.invert ? "invert" : ""
-              }`}
-              style={{ margin: "0 36px" }}
-              width={200}
-              height={100}
-              loading="lazy"
-            />
-          </a>
-        ))}
-      </Marquee>
-    </div>
+    <Marquee
+      gradient={true}
+      loop={0}
+      play={true}
+      autoFill={true}
+      pauseOnHover={true}
+      speed={50}
+      gradientColor="#1e2939"
+      style={{ zIndex: 20 }}
+    >
+      {clientLogos.map((logo, index) => (
+        <a
+          href={logo.link}
+          key={index}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="group flex justify-center align-middle py-2"
+        >
+          <img
+            src={logo.url}
+            alt={`${logo.name} logo`}
+            className={`min-w-12 md:max-h-20 max-w-24 md:max-w-52 w-full object-contain duration-300 ease-in-out group-hover:scale-110 group-hover:brightness-110 ${
+              logo.invert ? "invert" : ""
+            }`}
+            style={{ margin: "0 36px" }}
+            width={200}
+            height={100}
+            loading="lazy"
+          />
+        </a>
+      ))}
+    </Marquee>
   );
 };
 
